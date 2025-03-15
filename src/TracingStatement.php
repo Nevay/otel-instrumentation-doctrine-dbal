3445bc67c85290086285de65c5f63cb4f6d10ad0
Tobias Bachert <git@b-privat.de>
2025-03-15 23:12:17 +0100

    Use 0-based index for `db.operation.parameter`

--- src/TracingStatement.php
+++ src/TracingStatement.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Driver\Statement;
 use Doctrine\DBAL\ParameterType;
 use OpenTelemetry\API\Trace\SpanBuilderInterface;
 use function bin2hex;
+use function is_int;
 
 final class TracingStatement implements Statement {
 
@@ -19,15 +20,18 @@ final class TracingStatement implements Statement {
         $this->statement->bindValue($param, $value, $type);
 
         if ($this->config->captureParameters) {
-            $this->spanBuilder->setAttribute(sprintf('db.operation.parameter.%s', $param), match ($type) {
-                ParameterType::INTEGER,
-                ParameterType::ASCII,
-                ParameterType::STRING => (string) $value,
-                ParameterType::NULL => 'null',
-                ParameterType::BOOLEAN => $value ? 'true' : 'false',
-                ParameterType::BINARY => '0x' . bin2hex((string) $value),
-                ParameterType::LARGE_OBJECT => null,
-            });
+            $this->spanBuilder->setAttribute(
+                sprintf('db.operation.parameter.%s', is_int($param) ? $param - 1 : $param),
+                match ($type) {
+                    ParameterType::INTEGER,
+                    ParameterType::ASCII,
+                    ParameterType::STRING => (string) $value,
+                    ParameterType::NULL => 'null',
+                    ParameterType::BOOLEAN => $value ? 'true' : 'false',
+                    ParameterType::BINARY => '0x' . bin2hex((string) $value),
+                    ParameterType::LARGE_OBJECT => null,
+                },
+            );
         }
     }
 
